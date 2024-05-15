803dc66ac079b349b2e8f61e36832f9eaba3d315
Zigax201 <alvin.alamien@gmail.com>
2024-05-15 13:47:48 +0700

    feat require permission in every endpoint

--- src/main/java/com/bni/finproajubackend/controller/RoleController.java
+++ src/main/java/com/bni/finproajubackend/controller/RoleController.java
@@ -1,5 +1,6 @@
 package com.bni.finproajubackend.controller;
 
+import com.bni.finproajubackend.annotation.RequirePermission;
 import com.bni.finproajubackend.dto.role.RoleRequestDTO;
 import com.bni.finproajubackend.dto.role.RoleResponseDTO;
 import com.bni.finproajubackend.interfaces.RoleInterface;
@@ -21,18 +22,21 @@ public class RoleController {
     @Autowired
     private TemplateResInterface responseService;
 
+    @RequirePermission("getRoles")
     @GetMapping(value = "", produces = "application/json")
     public ResponseEntity getRoles(Authentication authentication){
         List<RoleResponseDTO> result = roleService.getRoles(authentication);
         return ResponseEntity.ok(responseService.apiSuccess(result));
     }
 
+    @RequirePermission("addRoles")
     @PostMapping(value = "", produces = "application/json")
     public ResponseEntity createNewRole(@RequestBody RoleRequestDTO roleRequestDTO) {
         RoleResponseDTO result = roleService.createNewRole(roleRequestDTO);
         return ResponseEntity.ok(responseService.apiSuccess(result));
     }
 
+    @RequirePermission("updateRoles")
     @PutMapping(value = "", produces = "application/json")
     public ResponseEntity updateRole(@RequestBody RoleRequestDTO roleRequestDTO) {
         RoleResponseDTO result = roleService.updateRole(roleRequestDTO);
--- src/main/java/com/bni/finproajubackend/controller/UserController.java
+++ src/main/java/com/bni/finproajubackend/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.bni.finproajubackend.controller;
 
+import com.bni.finproajubackend.annotation.RequirePermission;
 import com.bni.finproajubackend.dto.user.UserRequestDTO;
 import com.bni.finproajubackend.dto.user.UserResponseDTO;
 import com.bni.finproajubackend.interfaces.TemplateResInterface;
@@ -22,6 +23,7 @@ public class UserController {
     @Autowired
     private TemplateResInterface responseService;
 
+    @RequirePermission("getAllUser")
     @GetMapping(value = "", produces = "application/json")
     public ResponseEntity getUser() {
         List<User> listUser = userService.getUsers();
@@ -37,6 +39,7 @@ public class UserController {
         return ResponseEntity.ok(responseService.apiSuccess(userDetails));
     }
 
+    @RequirePermission("addUser")
     @PostMapping(value = "", produces = "application/json")
     public ResponseEntity addUser(@RequestBody UserRequestDTO req, Authentication authentication) {
         try {
@@ -47,6 +50,7 @@ public class UserController {
         }
     }
 
+    @RequirePermission("updateUser")
     @PutMapping(value = "/{username}", produces = "application/json")
     public ResponseEntity<User> editUser(@PathVariable String username, @RequestBody UserRequestDTO req) {
         User result = userService.editUser(username, req);
