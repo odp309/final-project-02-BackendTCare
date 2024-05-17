ca2f60d4679c29f2f87a48a16f153cb63ba8f561
Zigax201 <alvin.alamien@gmail.com>
2024-05-17 10:48:57 +0700

    Add Constructor

--- src/main/java/com/bni/finproajubackend/model/TokenRevocation.java
+++ src/main/java/com/bni/finproajubackend/model/TokenRevocation.java
@@ -3,16 +3,20 @@ package com.bni.finproajubackend.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.Date;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Builder
 @Entity
 @Table(name = "token_revocation_list")
 public class TokenRevocation {
-
     @Id
     private String token;
     private Date expirationTime;
