73b6776328d22a69c771652f7418d83fe83b2429
Zigax201 <alvin.alamien@gmail.com>
2024-05-17 11:36:32 +0700

    Add check expiration token before insert

--- src/main/java/com/bni/finproajubackend/service/TokenRevocationListService.java
+++ src/main/java/com/bni/finproajubackend/service/TokenRevocationListService.java
@@ -1,19 +1,22 @@
 package com.bni.finproajubackend.service;
 
 import com.bni.finproajubackend.interfaces.JWTInterface;
+import com.bni.finproajubackend.interfaces.TokenRevocationListInterface;
 import com.bni.finproajubackend.model.TokenRevocation;
 import com.bni.finproajubackend.repository.TokenRevocationRepository;
 import lombok.Getter;
+import org.apache.coyote.BadRequestException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Getter
 @Service
-public class TokenRevocationListService {
+public class TokenRevocationListService implements TokenRevocationListInterface {
 
     @Autowired
     private TokenRevocationRepository tokenRevocationRepository;
@@ -21,7 +24,11 @@ public class TokenRevocationListService {
     @Autowired
     private JWTInterface jwtService;
 
-    public void addToRevocationList(String token) {
+    @Override
+    public void addToRevocationList(String token) throws BadRequestException {
+        if (jwtService.extractExpiration(token).before(new Date()))
+            throw new BadRequestException("Token Expired");
+
         TokenRevocation tokenRevocation = TokenRevocation.builder()
                 .token(token)
                 .expirationTime(jwtService.extractExpiration(token))
@@ -29,10 +36,12 @@ public class TokenRevocationListService {
         tokenRevocationRepository.save(tokenRevocation);
     }
 
+    @Override
     public boolean isTokenRevoked(String token) {
         return tokenRevocationRepository.findByToken(token).isPresent();
     }
 
+    @Override
     public void removeToken(String token) {
         tokenRevocationRepository.deleteByToken(token);
     }
