7612d48d2c68c1c5cd4fb1df252262fa0b77dc1e
Zigax201 <alvin.alamien@gmail.com>
2024-05-17 10:25:20 +0700

    Fix model TRL

--- src/main/java/com/bni/finproajubackend/model/TokenRevocation.java
+++ src/main/java/com/bni/finproajubackend/model/TokenRevocation.java
@@ -3,16 +3,17 @@ package com.bni.finproajubackend.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Builder;
 import lombok.Data;
-
-import java.time.LocalDateTime;
+import java.util.Date;
 
 @Data
+@Builder
 @Entity
 @Table(name = "token_revocation_list")
 public class TokenRevocation {
 
     @Id
     private String token;
-    private LocalDateTime expirationTime;
+    private Date expirationTime;
 }
