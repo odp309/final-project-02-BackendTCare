9da2e9f3a3fe6c783845cea5e20cce0bce8ee125
Zigax201 <alvin.alamien@gmail.com>
2024-05-17 10:25:03 +0700

    Fix import

--- src/main/java/com/bni/finproajubackend/repository/TokenRevocationRepository.java
+++ src/main/java/com/bni/finproajubackend/repository/TokenRevocationRepository.java
@@ -1,9 +1,16 @@
 package com.bni.finproajubackend.repository;
 
 import com.bni.finproajubackend.model.TokenRevocation;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-public interface TokenRevocationRepository {
+@Repository
+public interface TokenRevocationRepository extends JpaRepository<TokenRevocation, Long> {
     Optional<TokenRevocation> findByToken(String token);
+    boolean isTokenRevoked(String token);
+    @Transactional
+    boolean deleteByToken(String token);
 }
