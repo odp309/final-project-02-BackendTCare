90749f837be633001d076f517820fb8209330420
Dimas Aji Pangestu <daji18201@gmail.com>
2024-06-03 09:11:20 +0700

    create user mutation service

---
+++ src/main/java/com/bni/finproajubackend/service/UserMutationService.java
@@ -0,0 +1,67 @@
+package com.bni.finproajubackend.service;
+
+import com.bni.finproajubackend.dto.userAccount.TransactionDTO;
+import com.bni.finproajubackend.dto.userAccount.UserMutationDTO;
+import com.bni.finproajubackend.interfaces.UserMutationInterface;
+import com.bni.finproajubackend.model.user.Person;
+import com.bni.finproajubackend.model.user.User;
+import com.bni.finproajubackend.model.user.nasabah.Account;
+import com.bni.finproajubackend.model.user.nasabah.Nasabah;
+import com.bni.finproajubackend.model.user.nasabah.Transaction;
+import com.bni.finproajubackend.repository.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class UserMutationService implements UserMutationInterface {
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private PersonRepository personRepository;
+    @Autowired
+    private TransactionRepository transactionRepository;
+    @Autowired
+    private NasabahRepository nasabahRepository;
+    @Autowired
+    private AccountRepository accountRepository;
+
+    @Override
+    public UserMutationDTO getUserMutations(Authentication authentication) {
+        String username = authentication.getName();
+        User user = userRepository.findByUsername(username);
+
+        if (user == null) {
+            throw new RuntimeException("User not found");
+        }
+
+        Person person = personRepository.findByUser(user);
+
+        if (person == null) {
+            throw new RuntimeException("Something went wrong!");
+        }
+        Nasabah nasabah = nasabahRepository.findByPerson(person);
+        //Transaction transaction = transactionRepository.findByNasabah(nasabah);
+        Account account = accountRepository.findByNasabah(nasabah);
+
+        List<Transaction> transactions = transactionRepository.findByAccount(account);
+        List<TransactionDTO> transactionDTOList = transactions.stream()
+                .map(this::convertToTransactionDTO)
+                .collect(Collectors.toList());
+
+        return UserMutationDTO.builder()
+                .transactionList(transactionDTOList)
+                .build();
+    }
+
+    private TransactionDTO convertToTransactionDTO(Transaction transaction){
+        TransactionDTO transactionDTO = new TransactionDTO();
+        transactionDTO.setId(transaction.getId());
+        transactionDTO.setTransactionDetail(transaction.getDetail());
+        transactionDTO.setStatus(transaction.getStatus());
+        return transactionDTO;
+    }
+}
