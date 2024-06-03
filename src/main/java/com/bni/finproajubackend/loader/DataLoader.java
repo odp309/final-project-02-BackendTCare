cb1bfc68b969181c8189c7543304a853c7f9fa6a
Zigax201 <alvin.alamien@gmail.com>
2024-06-03 14:34:50 +0700

    fix loader

--- src/main/java/com/bni/finproajubackend/loader/DataLoader.java
+++ src/main/java/com/bni/finproajubackend/loader/DataLoader.java
@@ -1,9 +1,7 @@
 package com.bni.finproajubackend.loader;
 import com.bni.finproajubackend.model.enumobject.Gender;
-import com.bni.finproajubackend.model.user.Person;
 import com.bni.finproajubackend.model.user.User;
 import com.bni.finproajubackend.model.user.admin.Admin;
-import com.bni.finproajubackend.model.user.admin.Permission;
 import com.bni.finproajubackend.model.user.admin.Role;
 import com.bni.finproajubackend.repository.*;
 import org.springframework.boot.CommandLineRunner;
@@ -11,25 +9,20 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
-import java.util.Collections;
 import java.util.Optional;
 
 @Component
 public class DataLoader {
 
     private final UserRepository userRepository;
-    private final PersonRepository personRepository;
     private final AdminRepository adminRepository;
     private final RoleRepository roleRepository;
-    private final PermissionRepository permissionRepository;
     private final PasswordEncoder passwordEncoder;
 
-    public DataLoader(UserRepository userRepository, PersonRepository personRepository, AdminRepository adminRepository, RoleRepository roleRepository, PermissionRepository permissionRepository, PasswordEncoder passwordEncoder) {
+    public DataLoader(UserRepository userRepository, AdminRepository adminRepository, RoleRepository roleRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
-        this.personRepository = personRepository;
         this.adminRepository = adminRepository;
         this.roleRepository = roleRepository;
-        this.permissionRepository = permissionRepository;
         this.passwordEncoder = passwordEncoder;
     }
 
@@ -46,41 +39,32 @@ public class DataLoader {
             return;
         }
 
-        // Menambahkan permission
-        Permission permission = new Permission();
-        permission.setPermissionName("all");
-        permission.setPermissionDescription("Bisa melakukan semuanya");
-        permissionRepository.save(permission);
-
         // Menambahkan role
         Role role = new Role();
         role.setRoleName("admin");
         role.setRoleDescription("Role untuk admin");
-        role.setPermissions(Collections.singletonList(permission));
         roleRepository.save(role);
 
         // Menambahkan user
         User user = new User("admin", passwordEncoder.encode("123456"));
-
-        // Menambahkan person
-        Person person = new Person();
-        person.setFirstName("Jakarta");
-        person.setAge(25);
-        person.setEmail("ex@gmail.com");
-        person.setLastName("zigax");
-        person.setGender(Gender.Male);
-        person.setAddress("arcadia");
-        person.setNoHP("081249149989");
-        person.setUser(user);
-        user.setPerson(person);
         userRepository.save(user);
-        personRepository.save(person);
 
         // Menambahkan admin
         Admin admin = new Admin();
         admin.setNpp("T094459");
-        admin.setPerson(person);
+        admin.setFirstName("aju");
+        admin.setLastName("agoy");
+        admin.setEmail("aju@gmail.com");
+        admin.setGender(Gender.Male);
+        admin.setAddress("Jakarta");
+        admin.setNoHP("082526365969");
+        admin.setAge(20);
         admin.setRole(role);
+        admin.setUser(user);
         adminRepository.save(admin);
+
+        user.setAdmin(admin);
+        userRepository.save(user);
+
     }
 }
