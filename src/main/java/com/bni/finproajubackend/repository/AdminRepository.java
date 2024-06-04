4682de485c6e1313db014d983d6383ffcf5ce104
Dimas Aji Pangestu <daji18201@gmail.com>
2024-06-04 20:06:32 +0700

    add findByUsername with its query

--- src/main/java/com/bni/finproajubackend/repository/AdminRepository.java
+++ src/main/java/com/bni/finproajubackend/repository/AdminRepository.java
@@ -2,9 +2,12 @@ package com.bni.finproajubackend.repository;
 
 import com.bni.finproajubackend.model.user.admin.Admin;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AdminRepository extends JpaRepository<Admin, Long> {
     Admin findByNpp(String npp);
+    @Query("SELECT a FROM Admin a WHERE a.user.username = :username")
+    Admin findByUsername(String username);
 }
