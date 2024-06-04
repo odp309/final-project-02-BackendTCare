3241ffe100c386a66fafe1ed574d7140f4eaf9bc
Dimas Aji Pangestu <daji18201@gmail.com>
2024-06-04 20:04:57 +0700

    add parameter Authentication authentication

--- src/main/java/com/bni/finproajubackend/interfaces/TicketInterface.java
+++ src/main/java/com/bni/finproajubackend/interfaces/TicketInterface.java
@@ -2,7 +2,8 @@ package com.bni.finproajubackend.interfaces;
 
 import com.bni.finproajubackend.model.enumobject.TicketStatus;
 import com.bni.finproajubackend.model.ticket.Tickets;
+import org.springframework.security.core.Authentication;
 
 public interface TicketInterface {
-    Tickets updateTicketStatus(Long ticketId, TicketStatus status);
+    Tickets updateTicketStatus(Long ticketId, TicketStatus status, Authentication authentication);
 }
