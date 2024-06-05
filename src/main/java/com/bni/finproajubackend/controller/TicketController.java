6a1116872f29034ab95424205911c740757ebac0
Dimas Aji Pangestu <daji18201@gmail.com>
2024-06-05 11:16:34 +0700

    adjusting the response

--- src/main/java/com/bni/finproajubackend/controller/TicketController.java
+++ src/main/java/com/bni/finproajubackend/controller/TicketController.java
@@ -1,18 +1,24 @@
 package com.bni.finproajubackend.controller;
 
 import com.bni.finproajubackend.annotation.RequiresPermission;
+import com.bni.finproajubackend.dto.TicketStatusResponseDTO;
 import com.bni.finproajubackend.dto.TicketStatusUpdateDTO;
 import com.bni.finproajubackend.dto.templateResponse.TemplateResponseDTO;
 import com.bni.finproajubackend.interfaces.TemplateResInterface;
+import com.bni.finproajubackend.model.enumobject.TicketStatus;
 import com.bni.finproajubackend.model.ticket.Tickets;
 import com.bni.finproajubackend.service.TicketService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
-@RequestMapping("/api/tickets")
+@RequestMapping("/api/v1/private/admin")
 public class TicketController {
 
     @Autowired
@@ -20,11 +26,19 @@ public class TicketController {
 
     @Autowired
     private TemplateResInterface responseService;
+    private Map<String, Object> errorDetails = new HashMap<>();
 
     @RequiresPermission("admin")
-    @PatchMapping("/{id}/status")
-    public ResponseEntity<TemplateResponseDTO<Tickets>> updateTicketStatus(@PathVariable Long id, @RequestBody TicketStatusUpdateDTO dto, Authentication authentication) {
-        Tickets result = ticketService.updateTicketStatus(id, dto.getStatus(), authentication );
-        return ResponseEntity.ok(responseService.apiSuccess(result, "Successfully updated ticket status."));
+    @PatchMapping("/{id}/update-status")
+    public ResponseEntity<TemplateResponseDTO<TicketStatusResponseDTO>> updateTicketStatus(@PathVariable Long id, @RequestBody TicketStatusUpdateDTO dto, Authentication authentication) {
+        try {
+            Tickets result = ticketService.updateTicketStatus(id, dto.getStatus(), authentication);
+            TicketStatus ticketStatus = result.getTicketStatus();
+            TicketStatusResponseDTO responseDTO = new TicketStatusResponseDTO(ticketStatus);
+            return ResponseEntity.ok(responseService.apiSuccess(responseDTO, "Successfully updated ticket status."));
+        } catch (Exception err) {
+            errorDetails.put("message", err.getCause() == null ? "Not Permitted" : err.getMessage());
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(responseService.apiFailed(null, err.getCause() == null ? "Something went wrong" : err.getMessage()));
+        }
     }
 }
