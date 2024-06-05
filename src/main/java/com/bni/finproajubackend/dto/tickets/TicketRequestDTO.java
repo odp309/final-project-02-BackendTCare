8a092ea7e7cfaa2da876143fb7156fa2d2e6dbc4
Muhammad Iqbal Naufal <iqbal010698@gmail.com>
2024-06-05 09:45:49 +0700

    create ticket request dto

--- src/main/java/com/bni/finproajubackend/dto/tickets/TicketRequestDTO.java
+++ src/main/java/com/bni/finproajubackend/dto/tickets/TicketRequestDTO.java
@@ -1,23 +1,16 @@
 package com.bni.finproajubackend.dto.tickets;
 
-import lombok.Builder;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.time.LocalDateTime;
-
 @Getter
 @Setter
 @Data
-@Builder
 public class TicketRequestDTO {
 
-    private Long id;
-    private Long transaction;
-    private Long ticketCategory;
-    private Long status;
+    private Long transactionId;
+    private Long ticketCategoryId;
     private String description;
-    private LocalDateTime responseTime;
 
 }
