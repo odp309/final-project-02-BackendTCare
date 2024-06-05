d7d5eb4841637bde5d877938a00fba41a4dff6e6
Dimas Aji Pangestu <daji18201@gmail.com>
2024-06-05 11:19:28 +0700

    add field category based on enum transaction categories

--- src/main/java/com/bni/finproajubackend/model/user/nasabah/Transaction.java
+++ src/main/java/com/bni/finproajubackend/model/user/nasabah/Transaction.java
@@ -1,5 +1,6 @@
 package com.bni.finproajubackend.model.user.nasabah;
 
+import com.bni.finproajubackend.model.enumobject.TransactionCategories;
 import com.bni.finproajubackend.model.ticket.Tickets;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
@@ -19,21 +20,32 @@ public class Transaction {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
     @ManyToOne
     @JoinColumn(name = "rekening_id", referencedColumnName = "id")
     private Account account;
+
     @Column(name = "transaction_detail")
     private String detail;
+
     @Column(name = "transaction_status")
     private String status;
+
     @Column(name = "amount")
     private Long amount;
+
+    @Column(name = "category") // Menambahkan properti untuk enum
+    @Enumerated(EnumType.STRING) // Menyimpan nilai enum sebagai string dalam database
+    private TransactionCategories category; // Properti yang menggunakan enum
+
     @OneToOne(mappedBy = "transaction")
     @JsonIgnore
     private Tickets tickets;
+
     @CreatedDate
     @Column(name = "created_at", updatable = false)
     private LocalDateTime createdAt;
+
     @LastModifiedDate
     @Column(name = "updated_at")
     private LocalDateTime updatedAt;
