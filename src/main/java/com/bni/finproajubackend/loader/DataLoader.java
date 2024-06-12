03cca6a3d7926c5ce39116d75481312d0668557c
Dimas Aji Pangestu <daji18201@gmail.com>
2024-06-12 20:01:28 +0700

    justify data loader

--- src/main/java/com/bni/finproajubackend/loader/DataLoader.java
+++ src/main/java/com/bni/finproajubackend/loader/DataLoader.java
@@ -120,16 +120,16 @@ public class DataLoader {
         userRepository.save(user9);
         userRepository.save(user10);
 
-        Nasabah nasabah = new Nasabah();
-        nasabah.setAddress("Balige");
-        nasabah.setAge(24);
-        nasabah.setEmail("daji18201@gmail.com");
-        nasabah.setFirst_name("Dimas");
-        nasabah.setGender(Gender.Male);
-        nasabah.setLast_name("Pangestu");
-        nasabah.setNik("1212784693778572");
-        nasabah.setNoHP("082265746357");
-        nasabah.setUser(user);
+        Nasabah nasabah1 = new Nasabah();
+        nasabah1.setAddress("Balige");
+        nasabah1.setAge(24);
+        nasabah1.setEmail("daji18201@gmail.com");
+        nasabah1.setFirst_name("Dimas");
+        nasabah1.setGender(Gender.Male);
+        nasabah1.setLast_name("Pangestu");
+        nasabah1.setNik("1212784693778572");
+        nasabah1.setNoHP("082265746357");
+        nasabah1.setUser(user);
 
         Nasabah nasabah2 = new Nasabah();
         nasabah2.setAddress("Pontianak");
@@ -230,7 +230,7 @@ public class DataLoader {
         nasabah10.setNoHP("082356781848");
         nasabah10.setUser(user10);
 
-        nasabahRepository.save(nasabah);
+        nasabahRepository.save(nasabah1);
         nasabahRepository.save(nasabah2);
         nasabahRepository.save(nasabah3);
         nasabahRepository.save(nasabah4);
@@ -241,87 +241,207 @@ public class DataLoader {
         nasabahRepository.save(nasabah9);
         nasabahRepository.save(nasabah10);
 
-        Account account = new Account();
-        account.setNasabah(nasabah);
-        account.setType("Rekening Tabungan");
-        account.setAccountNumber("1234567890");
-        account.setBalance(1000000L);
-        account.setCreatedAt(LocalDateTime.now());
-        account.setUpdatedAt(LocalDateTime.now());
+        Account account1 = new Account();
+        account1.setNasabah(nasabah1);
+        account1.setType("Rekening Tabungan");
+        account1.setAccountNumber("12345677890");
+        account1.setBalance(1000000L);
+        account1.setCreatedAt(LocalDateTime.now());
+        account1.setUpdatedAt(LocalDateTime.now());
 
         Account account2 = new Account();
-        account2.setNasabah(nasabah2);
+        account2.setNasabah(nasabah1);
         account2.setType("Rekening Tabungan");
-        account2.setAccountNumber("1234567790");
+        account2.setAccountNumber("12346567790");
         account2.setBalance(1000000L);
         account2.setCreatedAt(LocalDateTime.now());
         account2.setUpdatedAt(LocalDateTime.now());
 
         Account account3 = new Account();
-        account3.setNasabah(nasabah3);
+        account3.setNasabah(nasabah1);
         account3.setType("Rekening Tabungan");
-        account3.setAccountNumber("1232567790");
+        account3.setAccountNumber("14232567790");
         account3.setBalance(1000000L);
         account3.setCreatedAt(LocalDateTime.now());
         account3.setUpdatedAt(LocalDateTime.now());
 
         Account account4 = new Account();
-        account4.setNasabah(nasabah4);
+        account4.setNasabah(nasabah2);
         account4.setType("Rekening Tabungan");
-        account4.setAccountNumber("1834567890");
+        account4.setAccountNumber("18345678290");
         account4.setBalance(1000000L);
         account4.setCreatedAt(LocalDateTime.now());
         account4.setUpdatedAt(LocalDateTime.now());
 
         Account account5 = new Account();
-        account5.setNasabah(nasabah5);
+        account5.setNasabah(nasabah2);
         account5.setType("Rekening Tabungan");
-        account5.setAccountNumber("1734567890");
+        account5.setAccountNumber("17345167890");
         account5.setBalance(1000000L);
         account5.setCreatedAt(LocalDateTime.now());
         account5.setUpdatedAt(LocalDateTime.now());
 
         Account account6 = new Account();
-        account6.setNasabah(nasabah6);
+        account6.setNasabah(nasabah3);
         account6.setType("Rekening Tabungan");
-        account6.setAccountNumber("1234568890");
+        account6.setAccountNumber("12394568890");
         account6.setBalance(1000000L);
         account6.setCreatedAt(LocalDateTime.now());
         account6.setUpdatedAt(LocalDateTime.now());
 
         Account account7 = new Account();
-        account7.setNasabah(nasabah7);
+        account7.setNasabah(nasabah3);
         account7.setType("Rekening Tabungan");
-        account7.setAccountNumber("1234547890");
+        account7.setAccountNumber("12348547890");
         account7.setBalance(1000000L);
         account7.setCreatedAt(LocalDateTime.now());
         account7.setUpdatedAt(LocalDateTime.now());
 
         Account account8 = new Account();
-        account8.setNasabah(nasabah8);
+        account8.setNasabah(nasabah3);
         account8.setType("Rekening Tabungan");
-        account8.setAccountNumber("1534567890");
+        account8.setAccountNumber("15347567890");
         account8.setBalance(1000000L);
         account8.setCreatedAt(LocalDateTime.now());
         account8.setUpdatedAt(LocalDateTime.now());
 
         Account account9 = new Account();
-        account9.setNasabah(nasabah9);
+        account9.setNasabah(nasabah4);
         account9.setType("Rekening Tabungan");
-        account9.setAccountNumber("1134567890");
+        account9.setAccountNumber("11634567890");
         account9.setBalance(1000000L);
         account9.setCreatedAt(LocalDateTime.now());
         account9.setUpdatedAt(LocalDateTime.now());
 
         Account account10 = new Account();
-        account10.setNasabah(nasabah10);
+        account10.setNasabah(nasabah4);
         account10.setType("Rekening Tabungan");
-        account10.setAccountNumber("1930567890");
+        account10.setAccountNumber("19305675890");
         account10.setBalance(1000000L);
         account10.setCreatedAt(LocalDateTime.now());
         account10.setUpdatedAt(LocalDateTime.now());
 
-        accountRepository.save(account);
+        Account account11 = new Account();
+        account11.setNasabah(nasabah5);
+        account11.setType("Rekening Tabungan");
+        account11.setAccountNumber("193305647890");
+        account11.setBalance(1000000L);
+        account11.setCreatedAt(LocalDateTime.now());
+        account11.setUpdatedAt(LocalDateTime.now());
+
+        Account account12 = new Account();
+        account12.setNasabah(nasabah5);
+        account12.setType("Rekening Tabungan");
+        account12.setAccountNumber("19305367890");
+        account12.setBalance(1000000L);
+        account12.setCreatedAt(LocalDateTime.now());
+        account12.setUpdatedAt(LocalDateTime.now());
+
+        Account account13 = new Account();
+        account13.setNasabah(nasabah5);
+        account13.setType("Rekening Tabungan");
+        account13.setAccountNumber("19320567890");
+        account13.setBalance(1000000L);
+        account13.setCreatedAt(LocalDateTime.now());
+        account13.setUpdatedAt(LocalDateTime.now());
+
+        Account account14 = new Account();
+        account14.setNasabah(nasabah6);
+        account14.setType("Rekening Tabungan");
+        account14.setAccountNumber("19305617890");
+        account14.setBalance(1000000L);
+        account14.setCreatedAt(LocalDateTime.now());
+        account14.setUpdatedAt(LocalDateTime.now());
+
+        Account account15 = new Account();
+        account15.setNasabah(nasabah6);
+        account15.setType("Rekening Tabungan");
+        account15.setAccountNumber("19305667890");
+        account15.setBalance(1000000L);
+        account15.setCreatedAt(LocalDateTime.now());
+        account15.setUpdatedAt(LocalDateTime.now());
+
+        Account account16 = new Account();
+        account16.setNasabah(nasabah7);
+        account16.setType("Rekening Tabungan");
+        account16.setAccountNumber("19305567890");
+        account16.setBalance(1000000L);
+        account16.setCreatedAt(LocalDateTime.now());
+        account16.setUpdatedAt(LocalDateTime.now());
+
+        Account account17 = new Account();
+        account17.setNasabah(nasabah7);
+        account17.setType("Rekening Tabungan");
+        account17.setAccountNumber("19305678940");
+        account17.setBalance(1000000L);
+        account17.setCreatedAt(LocalDateTime.now());
+        account17.setUpdatedAt(LocalDateTime.now());
+
+        Account account18 = new Account();
+        account18.setNasabah(nasabah7);
+        account18.setType("Rekening Tabungan");
+        account18.setAccountNumber("19305647890");
+        account18.setBalance(1000000L);
+        account18.setCreatedAt(LocalDateTime.now());
+        account18.setUpdatedAt(LocalDateTime.now());
+
+        Account account19 = new Account();
+        account19.setNasabah(nasabah8);
+        account19.setType("Rekening Tabungan");
+        account19.setAccountNumber("19303567890");
+        account19.setBalance(1000000L);
+        account19.setCreatedAt(LocalDateTime.now());
+        account19.setUpdatedAt(LocalDateTime.now());
+
+        Account account20 = new Account();
+        account20.setNasabah(nasabah8);
+        account20.setType("Rekening Tabungan");
+        account20.setAccountNumber("19305467890");
+        account20.setBalance(1000000L);
+        account20.setCreatedAt(LocalDateTime.now());
+        account20.setUpdatedAt(LocalDateTime.now());
+
+        Account account21 = new Account();
+        account21.setNasabah(nasabah9);
+        account21.setType("Rekening Tabungan");
+        account21.setAccountNumber("19310567890");
+        account21.setBalance(1000000L);
+        account21.setCreatedAt(LocalDateTime.now());
+        account21.setUpdatedAt(LocalDateTime.now());
+
+        Account account22 = new Account();
+        account22.setNasabah(nasabah9);
+        account22.setType("Rekening Tabungan");
+        account22.setAccountNumber("19230567890");
+        account22.setBalance(1000000L);
+        account22.setCreatedAt(LocalDateTime.now());
+        account22.setUpdatedAt(LocalDateTime.now());
+
+        Account account23 = new Account();
+        account23.setNasabah(nasabah9);
+        account23.setType("Rekening Tabungan");
+        account23.setAccountNumber("19305657890");
+        account23.setBalance(1000000L);
+        account23.setCreatedAt(LocalDateTime.now());
+        account23.setUpdatedAt(LocalDateTime.now());
+
+        Account account24 = new Account();
+        account24.setNasabah(nasabah10);
+        account24.setType("Rekening Tabungan");
+        account24.setAccountNumber("19330567890");
+        account24.setBalance(1000000L);
+        account24.setCreatedAt(LocalDateTime.now());
+        account24.setUpdatedAt(LocalDateTime.now());
+
+        Account account25 = new Account();
+        account25.setNasabah(nasabah10);
+        account25.setType("Rekening Tabungan");
+        account25.setAccountNumber("19305267890");
+        account25.setBalance(1000000L);
+        account25.setCreatedAt(LocalDateTime.now());
+        account25.setUpdatedAt(LocalDateTime.now());
+
+        accountRepository.save(account1);
         accountRepository.save(account2);
         accountRepository.save(account3);
         accountRepository.save(account4);
@@ -331,11 +451,26 @@ public class DataLoader {
         accountRepository.save(account8);
         accountRepository.save(account9);
         accountRepository.save(account10);
+        accountRepository.save(account11);
+        accountRepository.save(account12);
+        accountRepository.save(account13);
+        accountRepository.save(account14);
+        accountRepository.save(account15);
+        accountRepository.save(account16);
+        accountRepository.save(account17);
+        accountRepository.save(account18);
+        accountRepository.save(account19);
+        accountRepository.save(account20);
+        accountRepository.save(account21);
+        accountRepository.save(account22);
+        accountRepository.save(account23);
+        accountRepository.save(account24);
+        accountRepository.save(account25);
 
         Bank bank = loadBank();
 
         for (int i = 1; i <= 50; i++) {
-            Transaction transaction = loadTransaction(account, bank, "Transaction " + i, 500000L + i, "Berhasil", TransactionCategories.values()[i % TransactionCategories.values().length]);
+            Transaction transaction = loadTransaction(account1, bank, "Transaction " + i, 500000L + i, "Berhasil", TransactionCategories.values()[i % TransactionCategories.values().length]);
             Transaction transaction2 = loadTransaction(account2, bank, "Transaction " + i, 500000L + i, "Berhasil", TransactionCategories.values()[i % TransactionCategories.values().length]);
             Transaction transaction3 = loadTransaction(account3, bank, "Transaction " + i, 500000L + i, "Berhasil", TransactionCategories.values()[i % TransactionCategories.values().length]);
             Transaction transaction4 = loadTransaction(account4, bank, "Transaction " + i, 500000L + i, "Berhasil", TransactionCategories.values()[i % TransactionCategories.values().length]);
@@ -345,6 +480,21 @@ public class DataLoader {
             Transaction transaction8 = loadTransaction(account8, bank, "Transaction " + i, 500000L + i, "Berhasil", TransactionCategories.values()[i % TransactionCategories.values().length]);
             Transaction transaction9 = loadTransaction(account9, bank, "Transaction " + i, 500000L + i, "Berhasil", TransactionCategories.values()[i % TransactionCategories.values().length]);
             Transaction transaction10 = loadTransaction(account10, bank, "Transaction " + i, 500000L + i, "Berhasil", TransactionCategories.values()[i % TransactionCategories.values().length]);
+            Transaction transaction11 = loadTransaction(account11, bank, "Transaction " + i, 500000L + i, "Berhasil", TransactionCategories.values()[i % TransactionCategories.values().length]);
+            Transaction transaction12 = loadTransaction(account12, bank, "Transaction " + i, 500000L + i, "Berhasil", TransactionCategories.values()[i % TransactionCategories.values().length]);
+            Transaction transaction13 = loadTransaction(account13, bank, "Transaction " + i, 500000L + i, "Berhasil", TransactionCategories.values()[i % TransactionCategories.values().length]);
+            Transaction transaction14 = loadTransaction(account14, bank, "Transaction " + i, 500000L + i, "Berhasil", TransactionCategories.values()[i % TransactionCategories.values().length]);
+            Transaction transaction15 = loadTransaction(account15, bank, "Transaction " + i, 500000L + i, "Berhasil", TransactionCategories.values()[i % TransactionCategories.values().length]);
+            Transaction transaction16 = loadTransaction(account16, bank, "Transaction " + i, 500000L + i, "Berhasil", TransactionCategories.values()[i % TransactionCategories.values().length]);
+            Transaction transaction17 = loadTransaction(account17, bank, "Transaction " + i, 500000L + i, "Berhasil", TransactionCategories.values()[i % TransactionCategories.values().length]);
+            Transaction transaction18 = loadTransaction(account18, bank, "Transaction " + i, 500000L + i, "Berhasil", TransactionCategories.values()[i % TransactionCategories.values().length]);
+            Transaction transaction19 = loadTransaction(account19, bank, "Transaction " + i, 500000L + i, "Berhasil", TransactionCategories.values()[i % TransactionCategories.values().length]);
+            Transaction transaction20 = loadTransaction(account20, bank, "Transaction " + i, 500000L + i, "Berhasil", TransactionCategories.values()[i % TransactionCategories.values().length]);
+            Transaction transaction21 = loadTransaction(account21, bank, "Transaction " + i, 500000L + i, "Berhasil", TransactionCategories.values()[i % TransactionCategories.values().length]);
+            Transaction transaction22 = loadTransaction(account22, bank, "Transaction " + i, 500000L + i, "Berhasil", TransactionCategories.values()[i % TransactionCategories.values().length]);
+            Transaction transaction23 = loadTransaction(account23, bank, "Transaction " + i, 500000L + i, "Berhasil", TransactionCategories.values()[i % TransactionCategories.values().length]);
+            Transaction transaction24 = loadTransaction(account24, bank, "Transaction " + i, 500000L + i, "Berhasil", TransactionCategories.values()[i % TransactionCategories.values().length]);
+            Transaction transaction25 = loadTransaction(account25, bank, "Transaction " + i, 500000L + i, "Berhasil", TransactionCategories.values()[i % TransactionCategories.values().length]);
 
             loadTickets(transaction);
             loadTickets(transaction2);
@@ -356,6 +506,21 @@ public class DataLoader {
             loadTickets(transaction8);
             loadTickets(transaction9);
             loadTickets(transaction10);
+            loadTickets(transaction11);
+            loadTickets(transaction12);
+            loadTickets(transaction13);
+            loadTickets(transaction14);
+            loadTickets(transaction15);
+            loadTickets(transaction16);
+            loadTickets(transaction17);
+            loadTickets(transaction18);
+            loadTickets(transaction19);
+            loadTickets(transaction20);
+            loadTickets(transaction21);
+            loadTickets(transaction22);
+            loadTickets(transaction23);
+            loadTickets(transaction24);
+            loadTickets(transaction25);
         }
     }
 
