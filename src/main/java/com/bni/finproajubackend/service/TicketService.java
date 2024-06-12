883546823ceeaf7de691387ce8ce3e503318a90d
Dimas Aji Pangestu <daji18201@gmail.com>
2024-06-12 09:45:04 +0700

    enable async at update ticket status

--- src/main/java/com/bni/finproajubackend/service/TicketService.java
+++ src/main/java/com/bni/finproajubackend/service/TicketService.java
@@ -14,6 +14,7 @@ import jakarta.transaction.Transactional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 import com.bni.finproajubackend.model.user.nasabah.Transaction;
