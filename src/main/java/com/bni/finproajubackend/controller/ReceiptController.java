4d520c6d1a0bf0ab28e4fb50e5edd8f1b4a8d5fd
Zigax201 <alvin.alamien@gmail.com>
2024-06-13 08:31:08 +0700

    feat: receipt or transaction detail

---
+++ src/main/java/com/bni/finproajubackend/controller/ReceiptController.java
@@ -0,0 +1,37 @@
+package com.bni.finproajubackend.controller;
+
+import com.bni.finproajubackend.dto.TicketStatusResponseDTO;
+import com.bni.finproajubackend.dto.receipt.TransactionResponseDTO;
+import com.bni.finproajubackend.dto.templateResponse.TemplateResponseDTO;
+import com.bni.finproajubackend.interfaces.ReceiptInterface;
+import com.bni.finproajubackend.interfaces.TemplateResInterface;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+import org.webjars.NotFoundException;
+
+@RestController
+@RequestMapping("/api/v1/private")
+public class ReceiptController {
+
+    @Autowired
+    private TemplateResInterface responseService;
+    @Autowired
+    private ReceiptInterface transactionService;
+
+    @GetMapping(value = "/customer/transaction/{id}/receipt", produces="application/json")
+    public ResponseEntity updateTicketStatus(@PathVariable Long id, Authentication authentication) {
+        try {
+            TransactionResponseDTO transactionResponseDTO = transactionService.getTransactionDetails(id, authentication);
+            return ResponseEntity.ok(responseService.apiSuccess(transactionResponseDTO, "Successfully updated ticket status."));
+        } catch (EntityNotFoundException err) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(responseService.apiNotFound(null, err.getCause() == null ? "Something went wrong" : err.getMessage()));
+        }catch (Exception err) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(responseService.apiFailed(null, err.getCause() == null ? "Something went wrong" : err.getMessage()));
+        }
+    }
+
+}
