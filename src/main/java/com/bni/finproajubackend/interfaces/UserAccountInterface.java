c6b5e108f033d22b3d816a43d57eae7df17ba094
Dimas Aji Pangestu <daji18201@gmail.com>
2024-06-13 12:54:26 +0700

    add enpoint account-number

--- src/main/java/com/bni/finproajubackend/interfaces/UserAccountInterface.java
+++ src/main/java/com/bni/finproajubackend/interfaces/UserAccountInterface.java
@@ -1,8 +1,10 @@
 package com.bni.finproajubackend.interfaces;
 
+import com.bni.finproajubackend.dto.userAccount.ListAccountNumberDTO;
 import com.bni.finproajubackend.dto.userAccount.UserAccountDTO;
 import org.springframework.security.core.Authentication;
 
 public interface UserAccountInterface {
     UserAccountDTO getUserAccount(Authentication authentication);
+    ListAccountNumberDTO getMyAccountNumber(Authentication authentication);
 }
