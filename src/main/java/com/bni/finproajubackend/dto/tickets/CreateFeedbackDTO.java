ce98b1f8839855c746cb181f004ac2c12f5d8832
Muhammad Iqbal Naufal <iqbal010698@gmail.com>
2024-06-14 14:02:31 +0700

    change logic on dto create feedback

--- src/main/java/com/bni/finproajubackend/dto/tickets/CreateFeedbackDTO.java
+++ src/main/java/com/bni/finproajubackend/dto/tickets/CreateFeedbackDTO.java
@@ -3,9 +3,13 @@ package com.bni.finproajubackend.dto.tickets;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
+@Getter
+@Setter
 @Data
 @Builder
 public class CreateFeedbackDTO {
