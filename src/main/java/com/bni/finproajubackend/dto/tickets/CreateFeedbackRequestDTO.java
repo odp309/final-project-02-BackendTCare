24a6dd4ae4de0eef34bf80e07cbb04b42dbbe740
Muhammad Iqbal Naufal <iqbal010698@gmail.com>
2024-06-19 15:14:55 +0700

    logic create feedback

--- src/main/java/com/bni/finproajubackend/dto/tickets/CreateFeedbackRequestDTO.java
+++ src/main/java/com/bni/finproajubackend/dto/tickets/CreateFeedbackRequestDTO.java
@@ -1,10 +1,13 @@
 package com.bni.finproajubackend.dto.tickets;
 
+import com.bni.finproajubackend.model.enumobject.StarRating;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.LastModifiedDate;
 
 import java.time.LocalDateTime;
 
@@ -12,12 +15,16 @@ import java.time.LocalDateTime;
 @Setter
 @Data
 @Builder
-public class CreateFeedbackDTO {
+public class CreateFeedbackRequestDTO {
+
+    private Long id;
+    @NotBlank(message = "Ticket number is mandatory")
     private String ticket_number;
-    private Integer rating;
+    private int rating;
     private String comment;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime created_at;
+    @LastModifiedDate
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime updated_at;
 }
--- src/main/java/com/bni/finproajubackend/dto/tickets/CreateFeedbackResponseDTO.java
+++ src/main/java/com/bni/finproajubackend/dto/tickets/CreateFeedbackResponseDTO.java
@@ -1,29 +1,44 @@
 package com.bni.finproajubackend.dto.tickets;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Enumerated;
 import lombok.Builder;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
+import org.jetbrains.annotations.Contract;
+import org.springframework.data.annotation.LastModifiedDate;
 
 import java.time.LocalDateTime;
 
-@Getter
 @Setter
+@Getter
 @Data
 @Builder
 public class CreateFeedbackResponseDTO {
-    private CreateFeedbackResponseDTO.ResultDTO result;
+    private FeedbackDetails result;
 
-    @Getter
+    public CreateFeedbackResponseDTO(FeedbackDetails result) {
+        this.result = result;
+    }
+
+    public CreateFeedbackResponseDTO() {
+
+    }
+
+    // Inner class for feedback details
     @Setter
-    public static class ResultDTO {
+    @Getter
+    public static class FeedbackDetails {
+        // Getters and Setters
         private String ticket_number;
         private int rating;
         private String comment;
         @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
-        private LocalDateTime created_at;
+        private LocalDateTime createdAt;
+        @LastModifiedDate
         @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
-        private LocalDateTime updated_at;
+        private LocalDateTime updatedAt;
     }
 }
