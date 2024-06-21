d8d27774b4dda50976a25aff126460b3a4389a70
Zigax201 <alvin.alamien@gmail.com>
2024-06-21 14:29:32 +0700

    Fix bug

--- src/main/java/com/bni/finproajubackend/repository/RoleRepository.java
+++ src/main/java/com/bni/finproajubackend/repository/RoleRepository.java
@@ -1,5 +1,6 @@
 package com.bni.finproajubackend.repository;
 
+import com.bni.finproajubackend.model.user.admin.Admin;
 import com.bni.finproajubackend.model.user.admin.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -10,4 +11,6 @@ import java.util.Optional;
 public interface RoleRepository extends JpaRepository<Role, Long> {
     Role findById(long id);
     Optional<Role> findByRoleName(String role);
+
+    Admin findByDivisionName(String divisionName);
 }
