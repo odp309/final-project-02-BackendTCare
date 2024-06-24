a04a1973b60b70a60b12920e501490cbb14857f3
Muhammad Iqbal Naufal <iqbal010698@gmail.com>
2024-06-24 14:45:27 +0700

    add pic to report detail for admin

--- src/main/java/com/bni/finproajubackend/dto/tickets/TicketDetailsReportDTO.java
+++ src/main/java/com/bni/finproajubackend/dto/tickets/TicketDetailsReportDTO.java
@@ -32,6 +32,7 @@ public class TicketDetailsReportDTO {
         private String category;
         private String description;
         private String reference_num;
+        private String pic;
     }
 
     @Data
